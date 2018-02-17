1d24165bb0d29d9d72c57fae98ae896fade66a1b
Ubuntu <ubuntu@dev.localdomain>
2018-02-17 19:54:00 +0000

    added test endpoints

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func GetPeople(w http.ResponseWriter, r *http.Request) {}
+func GetPerson(w http.ResponseWriter, r *http.Request) {}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
 func main(){
 	router := mux.NewRouter()
+	router.HandleFunc('/people", GetPeople).Methods("GET")
+	router.HandleFunc('/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc('/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc('/people/{id}", DeletePerson).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
